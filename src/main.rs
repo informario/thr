8c60c629445db9ce9a4bb58fc75a5067db1194e7
mario <hernanmlagarde@gmail.com>
2024-08-21 23:42:19 -0300

    refactor manejo errores

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,95 @@
 use core::time;
-use std::{env, io::{self, Read, Write}, net::{TcpListener, TcpStream}, sync::{Arc, Mutex}, thread, time::Duration};
-
+use std::{env, io::{self, Read, Write}, net::{TcpListener, TcpStream}, sync::{Arc, Mutex}, thread, time::Duration, error::Error};
+use gtk::{cairo::Result as CairoResult, gio::JoinHandle as GioJoinHandle, prelude::*};
+use gtk::{glib, Application};
+//const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
-    let mut handles = vec![];
+    let args = env::args().collect();
+    let mut handles: Vec<std::thread::JoinHandle<()>>  = vec![];
+    match bind_and_connect(&args, handles){
+        Ok(_) => (),
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
+
+fn bind_and_connect(args:&Vec<String>, mut handles:Vec<std::thread::JoinHandle<()>>) -> Result<(), Box<dyn Error>> {
     let listener;
     if args.len() > 1 {
         println!("Busco conectarme a: {}", args[1]);
-        let stream = TcpStream::connect((args[1].clone(), 8080)).unwrap();
-        stream.set_nonblocking(true).unwrap();
+        let stream = TcpStream::connect((args[1].clone(), 8080))?;
+        stream.set_nonblocking(true)?;
         let handle = thread::spawn(|| {
-            handle_connection(stream);
+            handle_connection(stream).expect("un HandleConnection se rompió");
         });
         handles.push(handle);
-        listener = TcpListener::bind("127.0.0.77:8080").unwrap();
+        listener = TcpListener::bind("127.0.0.77:8080")?;
         println!("Yo soy 127.0.0.77:8080")
     }
     else{
         println!("Solo recibo");
-        listener = TcpListener::bind("127.0.0.76:8080").unwrap();
+        listener = TcpListener::bind("127.0.0.76:8080")?;
         println!("Yo soy 127.0.0.76:8080")
     }
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        stream.set_nonblocking(true).unwrap();
+        let stream = stream?;
+        stream.set_nonblocking(true)?;
         let handle = thread::spawn(|| {
-            handle_connection(stream);
+            handle_connection(stream).expect("un HandleConnection se rompió");
         });
         handles.push(handle);
     }
     for handle in handles {
-        handle.join().unwrap(); // Esperamos a que cada hilo termine
+        handle.join().expect("Este hilo se rompió"); // Esperamos a que cada hilo termine
     }
+    return Ok(());
 }
 
 
-fn handle_connection(stream:TcpStream){
+fn handle_connection(stream:TcpStream)-> Result<(), Box<dyn Error>>{
     let arc_stream = Arc::new(Mutex::new(stream));
 
     let arc_stream_clone = Arc::clone(&arc_stream);
     let send_handle = thread::spawn(move || {
-        send(arc_stream_clone);
+        println!("{:?}", send(&arc_stream_clone));
     });
-
 
     let arc_stream_clone = Arc::clone(&arc_stream);
     let receive_handle = thread::spawn(move || {
-        receive(arc_stream_clone);
+        println!("{:?}", receive(&arc_stream_clone));
     });
-    send_handle.join().unwrap();    
-    receive_handle.join().unwrap();
+    send_handle.join().expect("Este hilo se rompió");  
+    receive_handle.join().expect("Este hilo se rompió");
+    return Ok(());
 }
 
-fn send(arc_stream:Arc<Mutex<TcpStream>>){
+fn send(arc_stream:&Arc<Mutex<TcpStream>>) -> Result<(), Box<dyn Error>> {
     loop {
-        println!("ss1");
         thread::sleep(time::Duration::from_millis(20));
-        println!("ss2");
-        let mut stream = arc_stream.lock().unwrap();
-        println!("ss3");
-        stream.write(b"adios").unwrap();
-        println!("ss4");
+        let mut stream = match arc_stream.lock(){
+            Ok(s) => s,
+            Err(_) => return Err("Error: obteniendo lock de stream".into()),
+        };
+        stream.write(b"adio")?;
     }
     }
 
-fn receive(arc_stream:Arc<Mutex<TcpStream>>){
-    loop {
-        println!("rr1");
-        thread::sleep(Duration::from_millis(20));
-        println!("rr2");
-        let mut stream = arc_stream.lock().unwrap();
-        println!("rr3");
+fn receive(arc_stream:&Arc<Mutex<TcpStream>>) -> Result<(), Box<dyn Error>> {
+    loop { 
+        thread::sleep(Duration::from_millis(20)); 
+        let mut stream = match arc_stream.lock(){
+            Ok(s) => s,
+            Err(_) => return Err("Error: obteniendo lock de stream".into()),
+        };
+
         let mut buf: [u8;6] = [0;6];
-        println!("rr4");
         match stream.read(&mut buf){
             Ok(_) => (),
             Err(_) => println!("error"),
         };
-        println!("rr5");
         println!("{:?}", &buf);
-        println!("rr6");
     }
 }
